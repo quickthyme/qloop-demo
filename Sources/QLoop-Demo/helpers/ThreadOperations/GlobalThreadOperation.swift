dbdb85bc9a46d649606523f9c743477e8f6c5ac1
Quickthyme <quickthyme@mac.com>
2019-03-10 23:25:18 -0500

    thread operation tests
    Signed-off-by: Quickthyme <quickthyme@mac.com>

--- Sources/QLoop-Demo/helpers/ThreadOperations/GlobalThreadOperation.swift
+++ Sources/QLoop-Demo/helpers/ThreadOperations/GlobalThreadOperation.swift
@@ -1,31 +1,6 @@
 
 import Foundation
 
-struct MainThreadOperation<Input> {
-    typealias Completion = (Input?) -> ()
-    typealias ErrCompletion = (Error) -> ()
-    typealias Operation = (Input?, @escaping Completion) -> ()
-    typealias Handler = (Error, @escaping Completion, @escaping ErrCompletion) -> ()
-
-    let id: String = "main_thread"
-
-    var op: Operation {
-        return ({ input, completion in
-            DispatchQueue.main.async {
-                completion(input)
-            }
-        })
-    }
-
-    var err: Handler {
-        return ({ error, _, errCompletion in
-            DispatchQueue.main.async {
-                errCompletion(error)
-            }
-        })
-    }
-}
-
 struct GlobalThreadOperation<Input> {
     typealias Completion = (Input?) -> ()
     typealias ErrCompletion = (Error) -> ()
@@ -58,5 +33,3 @@ struct GlobalThreadOperation<Input> {
         })
     }
 }
-
-
---
+++ Tests/QLoop-DemoTests/ThreadOperations/GlobalThreadOperationTests.swift
@@ -0,0 +1,50 @@
+
+import XCTest
+
+class GlobalThreadOperationTests: XCTestCase {
+    typealias Completion = (Int?) -> ()
+    typealias ErrCompletion = (Error) -> ()
+    typealias Operation = (Int?, @escaping Completion) -> ()
+    typealias Handler = (Error, @escaping Completion, @escaping ErrCompletion) -> ()
+
+    var subject: GlobalThreadOperation<Int>!
+
+    override func setUp() {
+        subject = GlobalThreadOperation(.background)
+    }
+
+    func test_has_id() {
+        XCTAssertEqual(subject.id, "global_qos_thread_background")
+    }
+
+    func test_calling_operation_calls_completion_on_background_thread() throws {
+        let expectBackgroundThread = expectation(description: "expectBackgroundThread")
+        var thread: Thread? = nil
+        let completion: Completion = { _ in
+            thread = Thread.current;
+            expectBackgroundThread.fulfill()
+        }
+
+        subject.op(1, completion)
+
+        wait(for: [expectBackgroundThread], timeout: 0.2)
+        XCTAssertNotNil(thread)
+        XCTAssertFalse(thread?.isMainThread ?? false)
+    }
+
+    func test_calling_handler_calls_error_completion_on_background_thread() throws {
+        let expectBackgroundThread = expectation(description: "expectBackgroundThread")
+        var thread: Thread? = nil
+        let completion: Completion = { _ in }
+        let errCompletion: ErrCompletion = { _ in
+            thread = Thread.current;
+            expectBackgroundThread.fulfill()
+        }
+
+        subject.err(MockError(), completion, errCompletion)
+
+        wait(for: [expectBackgroundThread], timeout: 0.2)
+        XCTAssertNotNil(thread)
+        XCTAssertFalse(thread?.isMainThread ?? false)
+    }
+}
