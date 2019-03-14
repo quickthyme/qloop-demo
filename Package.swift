135fc06cbbe6e24e2cdecca7ed2034b1acd8d1e2
Quickthyme <quickthyme@mac.com>
2019-03-13 20:38:47 -0500

    package correction
    Signed-off-by: Quickthyme <quickthyme@mac.com>

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "QLoop-Demo",
     dependencies: [
-        .package(url: "https://github.com/quickthyme/qloop.git", from: "0.0.5"),
+        .package(url: "https://github.com/quickthyme/qloop.git", from: "0.0.8"),
     ],
     targets: [
         .target(name: "QLoop-Demo", dependencies: ["QLoop"]),
