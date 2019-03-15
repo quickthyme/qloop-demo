c65b76c47494ac1cad1407ae95c061c28fcebcbe
Quickthyme <quickthyme@mac.com>
2019-03-15 02:31:21 -0500

    update submodule
    Signed-off-by: Quickthyme <quickthyme@mac.com>

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "QLoop-Demo",
     dependencies: [
-        .package(url: "https://github.com/quickthyme/qloop.git", from: "0.0.8"),
+        .package(url: "https://github.com/quickthyme/qloop.git", from: "0.1.0"),
     ],
     targets: [
         .target(name: "QLoop-Demo", dependencies: ["QLoop"]),
